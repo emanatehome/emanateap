6e9ac395fbfa8757b81b5d29154fc1dbe24e89fa
Nisar <emanatehome.social@gmail.com>
2025-08-27 15:49:37 +0530

    Added auth/login endpoint

--- app.py
+++ app.py
@@ -16,7 +16,3 @@ def login():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
-@app.route("/auth/login", methods=["POST"])
-def login():
-    return {"message": "This is login endpoint"}
-
